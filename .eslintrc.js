30b9f58a78d628363477248aba7dba413f462292
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-03-12 14:27:18 +0530

    tweaking the eslint config 🛠

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,57 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
+  "env": {
+    "es6": true,
+    "node": true
   },
-  extends: [
-    'airbnb-base',
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
+  "extends": [
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
-  rules: {
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "settings": {
+    "plugins": [
+      "import"
+    ],
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+  "rules": {
     "no-unused-vars": 0,
     "import/prefer-default-export": 0,
-    "@typescript-eslint/no-unused-vars": ["error"]
-  },
-};
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/type-annotation-spacing": ["error", {
+      "before": true,
+      "after": true,
+      "overrides": {
+        "colon": {
+          "before": false
+        }
+      }
+    }],
+    "no-dupe-class-members": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "ts": "never",
+      }
+    ]
+  }
+}
