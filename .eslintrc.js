3dc066508564dae3ecab493199b9d7b938420e46
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-03-12 14:04:44 +0530

    setting up eslint ‼️

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "no-unused-vars": 0,
+    "import/prefer-default-export": 0,
+    "@typescript-eslint/no-unused-vars": ["error"]
+  },
+};
