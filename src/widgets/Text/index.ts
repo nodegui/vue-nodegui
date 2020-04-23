a9e2ed96f584ae0d51fef8df5588c636e0da2c70
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-23 10:51:36 +0530

    cleaning up VNText 🧹

--- src/widgets/Text/index.ts
+++ src/widgets/Text/index.ts
@@ -3,20 +3,8 @@ import { VNText } from './VNText';
 class TextConfig {
     parentNode: any;
 
-    insertChild() {
-      console.log('insert child of text');
-    }
-
     createElement() {
       return new VNText();
-    }
-
-    setText(node: VNText, text: string) {
-      node.setText(text);
-    }
-
-    setElementText(node: VNText, text: string) {
-      node.setText(text);
     }
 }
 
