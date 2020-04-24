dba84a40ec3e2df636a545e4d02df2be7380847a
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-24 18:04:32 +0530

    creating types for prop setters 🔥

--- src/renderer/patchProp.ts
+++ src/renderer/patchProp.ts
@@ -1,4 +1,15 @@
 import { RendererOptions } from '@vue/runtime-core';
+import { NodeWidget } from '@nodegui/nodegui';
+
+export type Prop<T, key extends keyof T> = Required<T>[key];
+
+export type PropSetters<T extends {}> = {
+  [key in keyof Required<T>]: (
+    widget: NodeWidget<any>,
+    prevValue: Prop<T, key>,
+    nextValue: Prop<T, key>
+  ) => void
+}
 
 const patchProp: RendererOptions['patchProp'] = (
   el,
@@ -11,10 +22,7 @@ const patchProp: RendererOptions['patchProp'] = (
   // parentSuspense,
   // unmountChildren,
 ) => {
-  // TODO: handle prop additions in individual widgets
-  if (!prevValue) {
-    el.addEventListener('pressed', nextValue);
-  }
+  el.patchProp(key, prevValue, nextValue);
 };
 
 export default patchProp;
