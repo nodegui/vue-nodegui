7908e42c587307ea5fb55555f9f15cab6ee4785c
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-30 13:31:40 +0530

    adding image in the demo🌸

--- demo/Header.vue
+++ demo/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>{{line1Text}}</p>
+    <img src="https://via.placeholder.com/150" />
     <input @textChanged="textChanged" placeholderText="legend says you can type here" />
     <p>Second line of text {{count}}</p>
     <button v-on:clicked="inc" :flat="true">increment</button>
