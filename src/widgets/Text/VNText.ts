e127c7667de01bedc5e26c68488b2a023ac265d6
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-30 13:12:45 +0530

    adding view props to text widget 🔥

--- src/widgets/Text/VNText.ts
+++ src/widgets/Text/VNText.ts
@@ -1,5 +1,27 @@
 import { QLabel } from '@nodegui/nodegui';
+import { PropSetters, Prop } from '../../renderer/patchProp';
+import { ViewProps, viewPropsSetters } from '../View/VNView';
+
+export interface TextProps extends ViewProps {
+
+}
+
+
+export const textPropsSetters: PropSetters<VNText, TextProps> = {
+  ...viewPropsSetters,
+};
 
 export class VNText extends QLabel {
+  insertChild() {
+    throw new Error('Cannot add child to text elements');
+  }
 
+  patchProp(
+    key: keyof TextProps,
+    prevValue: Prop<TextProps, typeof key>,
+    nextValue: Prop<TextProps, typeof key>,
+  ) {
+    const propSetter = textPropsSetters[key];
+    if (propSetter !== undefined) { propSetter(this, prevValue as never, nextValue as never); }
+  }
 }
