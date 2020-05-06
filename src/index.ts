95bd3eed8b21a9a5d33babcff7272fe56f06486d
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-05-06 20:01:07 +0530

    adding v-model for vn-line-edit 🔥

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
+import { ObjectDirective } from '@vue/runtime-core';
+import { VNLineEdit } from 'widgets/LineEdit/VNLineEdit';
+
 export { createApp } from './renderer';
+
+type ModelDirective<T> = ObjectDirective<T & { _assign: Function }>
+
+export const vModelText: ModelDirective<VNLineEdit> = {
+  // @ts-ignore
+  beforeMount: (el, { value }, vnode) => {
+    el.setText(value);
+    // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+    el._assign = vnode.props!['onUpdate:modelValue'] as Function;
+    el.addEventListener('textChanged', (text) => {
+      // eslint-disable-next-line no-underscore-dangle
+      el._assign(text);
+    });
+  },
+  beforeUpdate: (el, { value, oldValue }, vnode) => {
+    if (value === oldValue) {
+      return;
+    }
+    el.setText(value);
+  },
+};
+
 export * from '@vue/runtime-core';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
+// eslint-disable-next-line
+const { transformModel: baseTransform, registerRuntimeHelpers } = require('@vue/compiler-core');
 const { isNativeWidget } = require('./dist/widgets/nativeWidget');
+
+const x = Symbol('vModelText');
+
+registerRuntimeHelpers({
+  [x]: 'vModelText',
+});
 
 module.exports = (env = {}) => ({
   mode: env.prod ? 'production' : 'development',
@@ -27,7 +35,19 @@ module.exports = (env = {}) => ({
             compilerOptions: {
               isNativeTag: isNativeWidget,
               directiveTransforms: {
-                model: console.log.bind(null, 'akkad bakkad'),
+                model: (dir, node, context) => {
+                  const baseResult = baseTransform(dir, node, context);
+                  const { tag } = node;
+                  let directiveToUse;
+                  if (tag === 'vn-line-edit') {
+                    directiveToUse = x;
+                  }
+                  baseResult.needRuntime = context.helper(directiveToUse);
+                  // console.log('dir', dir);
+                  // console.log('node', node);
+                  // console.log('context', context);
+                  return baseResult;
+                },
               },
             },
           },
