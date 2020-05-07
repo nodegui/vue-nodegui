bba3d0d34a8ed2bb1414c52c10e088fae713a8d5
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-05-07 17:12:31 +0530

    moving directives to their own folder 🧹

---
+++ src/renderer/directives/vModelText.ts
@@ -0,0 +1,22 @@
+import { ObjectDirective } from '@vue/runtime-core';
+import { VNLineEdit } from 'widgets/LineEdit/VNLineEdit';
+
+type ModelDirective<T> = ObjectDirective<T & { _assign: Function }>
+
+export const vModelText: ModelDirective<VNLineEdit> = {
+  beforeMount: (el, { value }, vnode) => {
+    el.setText(value);
+    // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+    el._assign = vnode.props!['onUpdate:modelValue'] as Function;
+    el.addEventListener('textChanged', (text) => {
+      // eslint-disable-next-line no-underscore-dangle
+      el._assign(text);
+    });
+  },
+  beforeUpdate: (el, { value, oldValue }) => {
+    if (value === oldValue) {
+      return;
+    }
+    el.setText(value);
+  },
+};
