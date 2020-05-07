14fc9b2e4e40efcc65a584c16f31ee76945c90e4
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-05-07 17:56:59 +0530

    using config from package in webpack build 🧹

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
-// eslint-disable-next-line
-const { transformModel: baseTransform, registerRuntimeHelpers } = require('@vue/compiler-core');
-const { isNativeWidget } = require('./dist/widgets/nativeWidget');
-
-const x = Symbol('vModelText');
-
-registerRuntimeHelpers({
-  [x]: 'vModelText',
-});
+const { compilerOptions } = require('./dist/vueLoader');
 
 module.exports = (env = {}) => ({
   mode: env.prod ? 'production' : 'development',
@@ -32,24 +24,7 @@ module.exports = (env = {}) => ({
         use: {
           loader: 'vue-loader',
           options: {
-            compilerOptions: {
-              isNativeTag: isNativeWidget,
-              directiveTransforms: {
-                model: (dir, node, context) => {
-                  const baseResult = baseTransform(dir, node, context);
-                  const { tag } = node;
-                  let directiveToUse;
-                  if (tag === 'vn-line-edit') {
-                    directiveToUse = x;
-                  }
-                  baseResult.needRuntime = context.helper(directiveToUse);
-                  // console.log('dir', dir);
-                  // console.log('node', node);
-                  // console.log('context', context);
-                  return baseResult;
-                },
-              },
-            },
+            compilerOptions,
           },
         },
       },
