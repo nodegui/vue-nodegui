517ceeedeb51cca534bfcca54ac5b7096540ac4c
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-09-08 10:33:47 +0530

    adding description sections to the main page 📚

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -75,13 +75,37 @@ function Home() {
           </div>
         </div>
       </header>
-      <main>
+      <main className={styles.content}>
         <section className={styles.features}>
           <div className="container">
             <div className="row">
               {features.map((props, idx) => (
                 <Feature key={idx} {...props} />
               ))}
+            </div>
+          </div>
+        </section>
+        <section className={styles.description}>
+          <div className={classnames("row", styles.descriptionRow)}>
+            <div className={classnames("col col--6", styles.imgColumn)}>
+              <img src="img/demo-calculator.png" className={styles.demoImg} />
+            </div>
+            <div className="col col--6">
+              <h3>Create native apps for Windows, MacOs and Linux using Vue and CSS</h3>
+              <p>lets you create truly native apps and doesn't compromise on your users' experience. It provides a core set of platform agnostic native widgets that map directly to the platform’s native UI building blocks.</p>
+              <p>Vue NodeGui widgets are built on top of <a href="https://www.qt.io/" target="_blank">Qt</a> which is a mature desktop apps framework. Vue NodeGui components are extremely customizable just like in the web but does <strong>NOT</strong> use a Web browser under the hood.</p>
+            </div>
+          </div>
+        </section>
+        <section className={styles.description}>
+          <div className={classnames("row", styles.descriptionRow)}>
+            <div className="col col--6">
+              <h3>Written in Vue — rendered with native code by Qt</h3>
+              <p>Apps can be built completely in JavaScript. This enables native app development for whole new teams of developers, and can let existing native teams work much faster.</p>
+              <p>With NodeGui you get flexibility of web and performance of Native desktop apps.</p>
+            </div>
+            <div className={classnames("col col--6", styles.imgColumn)}>
+              <img src="img/sample-code.png" className={styles.demoImg} />
             </div>
           </div>
         </section>
