e3b1f502a76f8001baf1b99fdf7e2ce486e4bbdf
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-09-27 11:49:57 +0530

    adding demo for scroll area widget 🌸

--- demo/App.vue
+++ demo/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <HelloWorld>
-    <template v-slot:header>
-      <vn-text>Here might be a page title</vn-text>
-    </template>
-    <vn-text>Default slot hello content</vn-text>
-  </HelloWorld>
+  <vn-scroll-area>
+    <HelloWorld>
+      <template v-slot:header>
+        <vn-text>Here might be a page title</vn-text>
+      </template>
+      <vn-text>Default slot hello content</vn-text>
+    </HelloWorld>
+  </vn-scroll-area>
 </template>
 
 <script>
-import HelloWorld from './Header';
+import HelloWorld from "./Header";
 
 export default {
   components: {
-    HelloWorld: HelloWorld
-  }
-}
+    HelloWorld: HelloWorld,
+  },
+};
 </script>
