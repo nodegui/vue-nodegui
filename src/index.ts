43d057c74b0f4176ad9a78273c73f9a24b95ea1c
Rohini Senthil <rohinivsenthil@gmail.com>
2020-10-02 17:46:00 +0530

    implementing v-model for spinbox

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { createApp } from './renderer';
 export { vModelText } from './renderer/directives/vModelText';
 export { vModelSlider } from './renderer/directives/vModelSlider';
+export { vModelSpinBox } from './renderer/directives/vModelSpinBox';
 export { vShow } from './renderer/directives/vShow';
 
 export * from '@vue/runtime-core';
