c39755030af92d5cdc3a08ebd16f0522f6909a6c
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-11-07 13:23:53 +0530

    using the value changed event instead of slider moved in slider model 🐞 (#36)

--- src/renderer/directives/vModelSlider.ts
+++ src/renderer/directives/vModelSlider.ts
@@ -1,14 +1,15 @@
 import { ObjectDirective } from '@vue/runtime-core';
+import { VNDial } from 'widgets/Dial/VNDial';
 import { VNSlider } from 'widgets/Slider/VNSlider';
 
 type ModelDirective<T> = ObjectDirective<T & { _assign: Function }>
 
-export const vModelSlider: ModelDirective<VNSlider> = {
+export const vModelSlider: ModelDirective<VNSlider | VNDial> = {
   beforeMount: (el, { value }, vnode) => {
     el.setValue(value);
     // eslint-disable-next-line no-param-reassign, no-underscore-dangle
     el._assign = vnode.props!['onUpdate:modelValue'] as Function;
-    el.addEventListener('sliderMoved', (sliderValue) => {
+    el.addEventListener('valueChanged', (sliderValue) => {
       // eslint-disable-next-line no-underscore-dangle
       el._assign(sliderValue);
     });
